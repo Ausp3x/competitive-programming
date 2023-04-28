9d8643408597b7630fc5260ede5329d8ca44cc7f
Ausp3x <imperatorindomitus@gmail.com>
2023-04-28 23:23:11 +0800

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   NOI 2023 Training/Week 0/Week 0.docx
    	new file:   README.md
    	new file:   input.txt
    	new file:   output.txt
    	new file:   template.cpp
    	new file:   template.exe

---
+++ README.md
@@ -0,0 +1,3 @@
+# https---github.com-Ausp3x-competitive-programming
+
+This repository is a compilation of all of my competitive programming work.
---
+++ template.cpp
@@ -0,0 +1,49 @@
+// 人外有人，天外有天
+// author: Ausp3x
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+typedef long long             lng;
+typedef unsigned int          uint;
+typedef unsigned long long    ulng;
+using namespace std;
+using namespace __gnu_pbds;
+
+// #define DEBUG
+
+void solve() {
+    
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+#ifdef DEBUG
+    auto start = chrono::high_resolution_clock::now();
+#endif
+
+#ifdef DEBUG
+    // testing code
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+#ifdef DEBUG
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout << duration.count() << " microseconds\n";
+#endif
+
+    return 0;
+}
